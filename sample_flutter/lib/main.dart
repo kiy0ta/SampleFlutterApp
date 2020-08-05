6b77d2ba3f5b82940a34391c9c8962b0cc17e0af
kiyota <m-kiyota@cries.co.jp>
2020-08-05 10:08:10 +0900

    # ウィジェットの説明コメントを追加

--- sample_flutter/lib/main.dart
+++ sample_flutter/lib/main.dart
@@ -11,19 +11,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
+        // actionBar、floatingActionButtonの色変更ができる
+        // colors.dartに定義してある
         primarySwatch: Colors.blue,
-
+        // おまじない？
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      // actionBarに表示するタイトル文言
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+/// StatefulWidgetはStateという状態を持つWidgetで動的に変化させることのできるWidgetであるのに対し、 (今回は数字を変化させたい)
+/// StatelessWidgetはStateをもたないWidgetであるため動的に変化をさせることができないWidgetとなります(今回はボタン)
 class MyHomePage extends StatefulWidget {
+  // インスタンス化？
   MyHomePage({Key key, this.title}) : super(key: key);
-
 
   final String title;
 
@@ -31,28 +35,34 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+/// カウントアップ処理
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    // setStateはStateの状態を更新するメソッドになります。
+    // 引数の関数を実行した後、Stateのbuildを実行しなおします。
     setState(() {
-
       _counter++;
     });
   }
 
+  /// Scaffoldの中身のウィジェットを定義している
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
+        // actionBarのタイトル文言
+        // 28行目のインスタンス化のときに引数でわたしている
+        // 21行目　home: MyHomePage(title: 'Flutter Demo Home Page')
+        // ここでタイトルを変更すると、21行目よりも55行目の文言が上書きされる
         title: Text(widget.title),
       ),
+      // Center = 中央揃えになる
+      // 詳細は「flutter Widget catalog」を参照
       body: Center(
-
+        // Columnは列なので縦にウィジェットが並ぶ、rowは行なので横一列に並ぶ
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -66,6 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        // 42行目のメソッド名
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
